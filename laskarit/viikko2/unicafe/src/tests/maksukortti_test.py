bf3530e15175fa9328e7afcdad68c9e4b75e7927
SouperSalamander <eeva.salo04@gmail.com>
2023-11-14 01:17:44 +0200

    new stuff

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,31 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_initial_correct(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_adds_correctly(self):
+        self.maksukortti.lataa_rahaa(2000)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 30.0)
+
+    def test_decreases_correctly(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5.0)
+
+    def test_no_change(self):
+        self.maksukortti.ota_rahaa(2000)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_true(self):
+        amount = self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(amount, True)
+    
+    def test_false(self):
+        amount = self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(amount, False)
