f1703a24daa41b4ebfd18c1b4e2fde205227dd1d
SouperSalamander <eeva.salo04@gmail.com>
2023-11-23 22:34:43 +0200

    added stuff

--- src/numberTheory.py
+++ src/numberTheory.py
@@ -1,10 +1,14 @@
 from tkinter import *
+from tkinter import messagebox
+import os
+import re 
 
 root = Tk()
 root.title("Number Theory")
 root.geometry("400x400")
 
-class Operations:
+#class to carry out gcf
+class gcfClass:
     def __init__(self, number1, number2):
         self.number1 = number1
         self.number2 = number2
@@ -35,6 +39,22 @@ class Operations:
         gcf_result = gcf_result + "The Greatest Common Factor is " + str(second)
         return(gcf_result)
 
+class lcmClass():
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
+
+    def lcm(self):
+        first = int(self.number1)
+        second = int(self.number2)
+        line1 = "Step 1: " + str(first) + " * " + str(second) + " = " + str(first*second)
+        line2 = "Step 2: Greatest Common Factor = " + gcfClass(first,second).gcf()[-1]
+        line3 = "Step 3: " + str(first*second) + " / " + gcfClass(first,second).gcf()[-1] + " = " + str((first*second)/(int(gcfClass(first,second).gcf()[-1])))
+        line4 = "The Lowest Common Multiple is " + str((first*second)/(int(gcfClass(first,second).gcf()[-1])))
+        lcm_result = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4
+        return(lcm_result)
+
+#shows greatest common factor frame
 def gcf():
     gcf_entryBox1.delete(0, END)
     gcf_entryBox2.delete(0, END)
@@ -42,19 +62,19 @@ def gcf():
     hide_frames()
     gcf_frame.pack(fill = "both", expand = 1)
 
+#shows lowest common factor frame
 def lcm():
     hide_words()
     hide_frames()
     lcm_frame.pack(fill = "both", expand = 1)
 
+#shows prime factorisation frame
 def pf():
     hide_words()
     hide_frames()
     pf_frame.pack(fill = "both", expand = 1)   
 
-def enterLogin():
-    pass
-
+#shows login frame
 def login():
     user_entry.delete(0, END)
     pass_entry.delete(0, END)
@@ -62,7 +82,7 @@ def login():
     hide_frames()
     login_frame.pack(fill = "both", expand = 1)
 
-def clear(gcf_add_answer):
+def gcfClear(gcf_add_answer):
     gcf_entryBox1.delete(0, END)
     gcf_entryBox2.delete(0, END)
     hide_words()
@@ -73,13 +93,89 @@ def clear(gcf_add_answer):
 def gcf_calculation():
     num1 = gcf_entryBox1.get()
     num2 = gcf_entryBox2.get()
-    gcf_add_answer = Label(gcf_frame, text = Operations(num1,num2).gcf())
+    gcf_add_answer = Label(gcf_frame, text = gcfClass(num1,num2).gcf())
     gcf_add_answer.grid(row = 6, column = 1, columnspan = 2)
-    gcf_clear = Button(gcf_frame, text = "clear", command = lambda: clear(gcf_add_answer))
+    gcf_clear = Button(gcf_frame, text = "clear", command = lambda: gcfClear(gcf_add_answer))
     gcf_clear.grid(row = 4, column = 3)
 
+def lcmClear(lcm_add_answer):
+    lcm_entryBox1.delete(0, END)
+    lcm_entryBox2.delete(0, END)
+    hide_words()
+    hide_frames()
+    lcm_frame.pack(fill = "both", expand = 1)
+    lcm_add_answer.destroy()
+
+def lcm_calculation():
+    num1 = lcm_entryBox1.get()
+    num2 = lcm_entryBox2.get()
+    lcm_add_answer = Label(lcm_frame, text = lcmClass(num1,num2).lcm())
+    lcm_add_answer.grid(row = 6, column = 1, columnspan = 2)
+    lcm_clear = Button(lcm_frame, text = "clear", command = lambda: lcmClear(lcm_add_answer))
+    lcm_clear.grid(row = 4, column = 3)
+
+#enter function for login
+def enterLogin():
+    correctDetails = False
+    user = user_entry.get()
+    password = pass_entry.get()
+    with open('userAccounts.txt', 'r') as l:
+        for line in l:
+            line = line.rstrip()
+            fullLine = line.split(",")
+            if user == fullLine[1] and password == fullLine[2]:
+                correctDetails = True
+                break
+    if correctDetails == False:
+        messagebox.showerror("Error", "Incorrect")
+    else:
+        mathMenu()
+
+#enter function for create account
+def enterCreate():
+    correctDetails = False
+    if os.path.getsize('userAccounts.txt') !=0:
+        with open('userAccounts.txt', 'r') as l:
+            list1 = l.readlines()
+            fullList = list1
+            lastLine = list1[-1]
+            lastLine = lastLine.rstrip()
+            line = lastLine.split(",")
+            uniqID = line[0]
+            uniqID = int(uniqID) + 1
+    else:
+        uniqID = 1
+    username = userCreate_entry.get()
+    pass1 = pass1_entry.get()
+    pass2 = pass2_entry.get()
+    if pass1 != pass2:
+        messagebox.showerror("Error", "Passwords don't match")
+    elif not re.search(r"[\d]+", pass1):
+        messagebox.showerror("Error", "Must contain a number")
+    elif not re.search("[A-Z]", pass1) and not re.search("[a-z]", pass1):
+        messagebox.showerror("Error", "Must contain a letter")
+    else:
+        with open('userAccounts.txt', 'r') as l:
+            for line in l:
+                line = line.rstrip()
+                fullLine = line.split(",")
+                if username == fullLine[1] and pass1 == fullLine[2]:
+                    correctDetails = True     
+        if correctDetails == True:
+            messagebox.showerror("Error", "Account Already Exists") 
+        else:
+            fileObject = open('userAccounts.txt', 'a')
+            fileObject.write(str(uniqID) + ',' + username + ',' + pass1 + '\n')
+            fileObject.close()
+            login()
+
 def create_account():
-    pass
+    userCreate_entry.delete(0, END)
+    pass1_entry.delete(0, END)
+    pass2_entry.delete(0, END)
+    hide_words()
+    hide_frames()
+    create_frame.pack(fill = "both", expand = 1)
 
 def mathMenu():
     hide_words()
@@ -155,6 +251,31 @@ back_btn.grid(row = 4, column = 2)
 back_btn = Button(create_frame, text = "BACK", command = back)
 back_btn.grid(row = 5, column = 2)
 
+#create account
+create_lbl = Label(create_frame, text = "Create Account", font = (None, 22))
+create_lbl.grid(row = 0, column = 1)
+
+userCreate = Label(create_frame, text = "Create Username:")
+userCreate.grid(row = 1, column = 0)
+
+userCreate_entry = Entry(create_frame)
+userCreate_entry.grid(row = 1, column = 1)
+
+pass1_lbl = Label(create_frame, text = "Create Password:")
+pass1_lbl.grid(row = 2, column = 0)
+
+pass1_entry = Entry(create_frame, show = "*")
+pass1_entry.grid(row = 2, column = 1)
+
+pass2_lbl = Label(create_frame, text = "Confirm Password:")
+pass2_lbl.grid(row = 3, column = 0)
+
+pass2_entry = Entry(create_frame, show = "*")
+pass2_entry.grid(row = 3, column = 1)
+
+enter_btn = Button(create_frame, text = "Enter", command = enterCreate)
+enter_btn.grid(row = 4, column = 1)
+
 #math menu
 menu_lbl = Label(menu_frame, text = "Menu", font = (None, 22))
 menu_lbl.pack()
@@ -172,17 +293,17 @@ logOut_btn = Button(menu_frame, text = "Log Out", command = back)
 logOut_btn.pack(pady = 3)
 
 #gcf frame
-title_lbl = Label(gcf_frame, text = "Greatest Common Factor")
-title_lbl.grid(row = 1, column = 1, columnspan = 2)
+gcf_lbl = Label(gcf_frame, text = "Greatest Common Factor")
+gcf_lbl.grid(row = 1, column = 1, columnspan = 2)
 
-first_lbl = Label(gcf_frame, text = "first number: ")
-first_lbl.grid(row = 2, column = 1)
+gcf_first_lbl = Label(gcf_frame, text = "first number: ")
+gcf_first_lbl.grid(row = 2, column = 1)
 
 gcf_entryBox1 = Entry(gcf_frame)
 gcf_entryBox1.grid(row = 2, column = 2)
 
-second_lbl = Label(gcf_frame, text = "second number: ")
-second_lbl.grid(row = 3, column = 1)
+gcf_second_lbl = Label(gcf_frame, text = "second number: ")
+gcf_second_lbl.grid(row = 3, column = 1)
 
 gcf_entryBox2 = Entry(gcf_frame)
 gcf_entryBox2.grid(row = 3, column = 2)
@@ -192,6 +313,28 @@ gcf_enter.grid(row = 4, column = 1, columnspan = 2)
 
 gcf_back = Button(gcf_frame, text = "back", command = mathMenu)
 gcf_back.grid(row = 5, column = 3)
+
+#lcm frame
+lcm_lbl = Label(lcm_frame, text = "Lowest Common Multiple")
+lcm_lbl.grid(row = 1, column = 1, columnspan = 2)
+
+lcm_first_lbl = Label(lcm_frame, text = "first number: ")
+lcm_first_lbl.grid(row = 2, column = 1)
+
+lcm_entryBox1 = Entry(lcm_frame)
+lcm_entryBox1.grid(row = 2, column = 2)
+
+lcm_second_lbl = Label(lcm_frame, text = "second number: ")
+lcm_second_lbl.grid(row = 3, column = 1)
+
+lcm_entryBox2 = Entry(lcm_frame)
+lcm_entryBox2.grid(row = 3, column = 2)
+
+lcm_enter = Button(lcm_frame, text = "Calculate", width = 30, command = lcm_calculation)
+lcm_enter.grid(row = 4, column = 1, columnspan = 2)
+
+lcm_back = Button(lcm_frame, text = "back", command = mathMenu)
+lcm_back.grid(row = 5, column = 3)
 
 root.mainloop()
 #<3
