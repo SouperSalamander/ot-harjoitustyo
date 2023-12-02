5ca5fb422cf9c9c5541c8ba3cd7a86042fd946f4
SouperSalamander <eeva.salo04@gmail.com>
2023-12-02 14:40:26 +0200

    new tests

--- src/tests/number_theory_test.py
+++ src/tests/number_theory_test.py
@@ -2,7 +2,9 @@ import unittest
 import os
 from src.number_theory import GcfClass
 from src.number_theory import LcmClass
+from src.number_theory import PfClass
 from src.number_theory import LoginClass
+from src.number_theory import AccountCreation
 
 class TestLogin(unittest.TestCase):
     def setUp(self):
@@ -22,10 +24,30 @@ class TestLogin(unittest.TestCase):
     def test_wrong_login(self):
         self.assertEqual(self.badInfo.login_check(), False)
 
+class TestAccountCreation(unittest.TestCase):
+    def setUp(self):
+        self.goodAccount = AccountCreation("accountTest","pass1","pass1")
+        self.badAccountNoMatch = AccountCreation("accountTest","pass","pass1")
+        self.badAccountNoNum = AccountCreation("accountTest","pass","pass")
+        self.badAccountNoAlph = AccountCreation("accountTest","1","1")
+
+    def test_good_account(self):
+        self.assertEqual(self.goodAccount.account_check(), (True,False,False,False,False))
+
+    def test_no_match(self):
+        self.assertEqual(self.badAccountNoMatch.account_check(), (False,False,True,False,False))
+
+    def test_no_num(self):
+        self.assertEqual(self.badAccountNoNum.account_check(), (False,False,False,True,False))
+
+    def test_no_letter(self):
+        self.assertEqual(self.badAccountNoAlph.account_check(), (False,False,False,False,True))
+
 class TestOperations(unittest.TestCase):
     def setUp(self):
         self.calc1 = GcfClass(12,18)
         self.calc2 = LcmClass(32,13)
+        self.calc3 = PfClass(9243)
 
     def test_gcf(self):
         class_return = list(self.calc1.gcf().split(" "))
@@ -36,3 +58,6 @@ class TestOperations(unittest.TestCase):
         class2_return = list(self.calc2.lcm().split(" "))
         lcm_value = class2_return[len(class2_return)-1]
         self.assertEqual(lcm_value, "416")
+
+    def test_pf(self):
+        self.assertEqual(self.calc3.pf(), [3, 3, 13, 79])
