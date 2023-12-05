6fef9bd58020483d0a7c5ddb53b76786a7bc394d
SouperSalamander <eeva.salo04@gmail.com>
2023-12-05 16:58:54 +0200

    task edit

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from invoke import task
 
 @task
 def start(ctx):
-    ctx.run("python3 number_theory_ui.py", pty = True)
+    ctx.run("uipython3 src/number_theory_ui.py", pty = True)
 
 @task
 def test(ctx):
